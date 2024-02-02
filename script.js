348662866f51bde6108a090687cb718154f2c936
Rachit Mahajan <141140128+theonly-rm@users.noreply.github.com>
2024-02-02 12:47:07 +0530

    added start button

--- script.js
+++ script.js
@@ -1,62 +1,79 @@
-let score = 0;
-let hit;
+document.addEventListener('DOMContentLoaded', function () {
 
-document.querySelector(".pbody").addEventListener("click",function(dets){
-    console.log(dets.view.innerHeight)
-})
+    function initiate() {
+        document.querySelector(".pbody").innerHTML = `<h3>Instructions:</h3>
+    <ol>
+        <li>Check the Hit count and hit the bubble with that number</li>
+        <li>As you hit the bubble with the correct number, your score will increase!</li>
+        <li>You get a total of 60 seconds to play and score</li>
+        <li>Click on the start button to play!</li>
+    </ol>
+    <button class="button">Let's Start</button>`
 
-function makeBubbles(){
-    let body = document.querySelector(".pbody");
-    let clutter = "";
-    let numberOfBubbles;
-
-    if (window.innerWidth >= 600) {
-        numberOfBubbles = 177; 
-    } else {
-        numberOfBubbles = 78; 
+    let button = document.querySelector(".button");
+    if (button) {
+        button.addEventListener("click", start);
     }
-    for(i=2;i<=numberOfBubbles;i++) {
-            clutter += `<div class="bubble">
-            ${Math.floor(Math.random()*10)}
-        </div>`
     }
-    body.innerHTML = clutter;
+
     
-}
 
-function updateHit(){
-    hit = Math.floor(Math.random()*10);
-    document.querySelector(".hit").textContent = hit;
-    
-}
+    initiate();
 
-function runTimer(){
-    let time = 60;
-        let timeInt = setInterval(function(){
-            if(time>0){
-                time--;
-                document.querySelector(".timer").innerHTML = time;
+    function start() {
+        let score = 0;
+        let hit;
+
+        updateHit();
+        makeBubbles();
+        runTimer();
+
+        function makeBubbles() {
+            let body = document.querySelector(".pbody");
+            let clutter = "";
+            let numberOfBubbles;
+
+            if (window.innerWidth >= 600) {
+                numberOfBubbles = 177;
+            } else {
+                numberOfBubbles = 78;
             }
-            else {
-                clearInterval(timeInt);
-                document.querySelector(".pbody").innerHTML = `<h1>Game Over! </h1> <br> <h1>Your Final Score : ${score}</h1>`
+            for (let i = 2; i <= numberOfBubbles; i++) {
+                clutter += `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`
             }
-        },1000);
-}
-
-function incScore(){
-        score+=10;
-        document.querySelector(".scoreVal").innerHTML = score;
-}
-
-document.querySelector(".pbody").addEventListener("click", function(dets){
-        if(hit == Number(dets.target.textContent)) {
-            incScore();
-            updateHit();
-            makeBubbles();
+            body.innerHTML = clutter;
         }
-})
 
-updateHit();
-runTimer();
-makeBubbles();
+        function updateHit() {
+            hit = Math.floor(Math.random() * 10);
+            document.querySelector(".hit").textContent = hit;
+        }
+
+        function runTimer() {
+            let time = 60;
+            let timeInt = setInterval(function () {
+                if (time > 0) {
+                    time--;
+                    document.querySelector(".timer").innerHTML = time;
+                } else {
+                    clearInterval(timeInt);
+                    document.querySelector(".pbody").innerHTML = `<h1>Game Over! </h1> <br> <h1>Your Final Score : ${score}</h1>`
+                }
+            }, 1000);
+        }
+
+        function incScore() {
+            score += 10;
+            document.querySelector(".scoreVal").innerHTML = score;
+        }
+
+        document.querySelector(".pbody").addEventListener("click", function (dets) {
+            if (hit == Number(dets.target.textContent)) {
+                incScore();
+                updateHit();
+                makeBubbles();
+            }
+        })
+    }
+
+});
